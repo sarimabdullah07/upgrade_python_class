0086c820d52613230bdb0ac94591b37d219bd047
sarimabdullah07 <sarimabdullah242@gmail.com>
2026-09-05 18:58:00 +0530

    Completion of making guessing game.

--- guessing_game.py
+++ guessing_game.py
@@ -1,13 +1,17 @@
 import random
 hidden_num=random.randint(1,100)
-print(hidden_num)
 score=100
-
 for i in range(5):
     guess=int(input("Guess a number between 1 to 100: "))
     if guess==hidden_num:
         print("You Guessed a right number.")
         print("score: ",score)
+        break
     elif guess<hidden_num:
-        pass
-
+        print("You guessed a low number")
+        score-=20
+    else:
+        print("You guessed a high number")
+        score-=20
+else:
+    print("You lost all the chances")
