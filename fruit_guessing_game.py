91639b280c3750c2b6f6e23396712a9a815038fd
unknown <sarimabdullah242@gmail.com>
2026-09-07 11:53:17 +0530

    Modification in fruit_guessing_game

--- fruit_guessing_game.py
+++ fruit_guessing_game.py
@@ -1,27 +1,26 @@
 import random
-list=["Mango","Banana","Kiwi","Apple","Orange","Pineapple","Watermelon","Date","Fig","Muskmelon","Guava","Grapes","Papaya","Cherry"]
+list=["mango","banana","kiwi","apple","orange","pineapple","watermelon","date","fig","muskmelon","guava","grapes","papaya","cherry"]
 fruit_name=random.choice(list)
-print(fruit_name)
 i=0
 point=100
 while(i<5):
-    guess=input("Guess any fruit name: ")
+    guess=input("Guess the fruit name: ")
     if guess==fruit_name:
-        print("You Won!")
+        print("Congratulations, You Won!")
         break
     else:
         if(i==0):
             print("Hint1: Fruit name starts with: ", fruit_name[0])
         elif(i==1):
-            print("Hint2: Length of fruit name is ",len(fruit_name)," letters.")
+            print("Hint2: Length of fruit name is ",len(fruit_name),"letters.")
         elif(i==2):
-            b=fruit_name[0]+"_"*(len(fruit_name)-2)+fruit_name[-1]
+            b="_"*(len(fruit_name)-1)+fruit_name[-1]
             print("Hint4: It could be: ",b)
         elif(i==3):
-            c=fruit_name[0]+fruit_name[1]+"_"*(len(fruit_name)-2)+fruit_name[-1]
-            print("Hint5: Abe Anpadh kaheen ke...",c)
+            c=fruit_name[0]+"_"*(len(fruit_name)-3)+fruit_name[-2]+fruit_name[-1]
+            print("Hint5: It's easy, guess fast...",c)
         else:
-            print("Ja ghar ja tu....",fruit_name,"ye hai.")
+            print("You Lost, the fruit is :",fruit_name)
     i+=1
     point-=20
 print("Score: ",point)
