4d0670d39d0525b5495cb25765421e721a54968d
unknown <sarimabdullah242@gmail.com>
2026-09-11 19:33:01 +0530

    rock_paper_scissor

--- RPS_game.py
+++ RPS_game.py
@@ -2,29 +2,18 @@ import random
 list=["rock","paper","scissor"]
 score=100
 i=0
+out_off=100
 while(i<5):
     r=random.choice(list)
     g=input("\n\nEnter: [rock, paper, scissor]\n")
     if(g==r):
         print("We guessed the same thing,",g)
-    elif((r=="rock")and(g=="paper")):
-        print("You win")
-        print("I gussed",r)
-    elif((r=="rock")and(g=="scissor")):
-        print("You lost")
         score-=20
-        print("I gussed",r)
-    elif((r=="paper")and(g=="rock")):
-        print("You lost")
-        score-=20
-        print("I gussed",r)
-    elif((r=="paper")and(g=="scissor")):
+        out_off-=20
+    elif((r=="rock")and(g=="paper") or (r=="paper")and(g=="scissor") or (r=="scissor")and(g=="rock")):
         print("You win")
         print("I gussed",r)
-    elif((r=="scissor")and(g=="rock")):
-        print("You win")
-        print("I gussed",r)
-    elif((r=="scissor")and(g=="paper")):
+    elif((r=="rock")and(g=="scissor") or (r=="paper")and(g=="rock") or (r=="scissor")and(g=="paper")):
         print("You lost")
         score-=20
         print("I gussed",r)
@@ -32,4 +21,4 @@ while(i<5):
         print("Enter proper input as rock,paper,scissor")
     pass
     i+=1
-print("\nyour score:",score,"out of 100")
+print("\nyour score:",score,"out of",out_off)
