0c70eb02f929a3cff7133952788667e57b43c635
XYZ <sumayyasadaf2004@gmail.com>
2026-09-14 23:06:32 +0530

    types_of _arguments

--- argument_types.py
+++ argument_types.py
@@ -46,12 +46,36 @@ Prefixed with a double asterisk(**), this collects any number
 of extra keyword arguments into a standard dictionary
 '''
 
-# positional+arbitray keyword arguments
+# positional + arbitray keyword arguments 
 def info(name,**data):
     print("\n",name," = ",data)
 info("Sarim",roll_no=812,marks=872,percentage=93.47)
 
-# positional+arbitray posional arguments
+# positional + arbitray posional arguments (*args)
 def info(box_num,*fruit):
     print("\n",box_num," = ",fruit)
 info(107,"Apple","Mango","Dates","Orange")
+
+
+# positional + default argument
+def just(name,city="malegaon"):
+    print("\nHi! ",name)
+    print("Are you from,", city,"?")
+just("Abdullah")
+just("Aqueel","Mumbai")
+'''
+A mix where optional parameters are assigned a fallback value using the assignment
+operator(=). Required parameters must occupy the left side. Optional default parameters 
+must occupy the right side.
+'''
+
+# The Complete Mix
+def all(name,*marks,**mws):
+    print("\nName: ",name)
+    print("marks: ",marks, "\nPercentage= ",(sum(marks)/len(marks)))
+    print("marks with subject: ",mws)
+all("abdurrahman",87,96,89,79,92,english=87,math=96,chemistry=89,biology=79,physices=92)
+'''
+The standard sequencing structure required when mixing all three types. 
+Python strictly requires this order: fixed positional then (*args) and at last (**kwargs).
+'''
