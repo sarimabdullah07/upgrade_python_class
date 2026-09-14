54e428980f99c16be00011d712d3f44e05bb6f5b
sarimabdullah07 <sarimabdullah242@gmail.com>
2026-09-14 21:59:48 +0530

    Add files via upload

--- argument_types.py
+++ argument_types.py
@@ -16,14 +16,42 @@ we can change the position of arguments by using keywords,
 note: The keywords should match the actual argument (parameter) names
 '''
 
-# arbitrary argument
-def area_of_circle(name,*marks):
-    total=sum(marks)
-    print("\n",name,"\n",marks,"\n","Total_Marks= ",total)
-area_of_circle("Circle",23,55,76,87,90)
+#default argument
+def greet(name, a="assalamualikum"):
+    print(name)
+    print(a)
+greet("\n sarim")
+'''
+If you omit these arguments during a function call,
+python uses the default value. If you pass a value, it overrides
+the default
+'''
 
-# keyworded arbitrary argument
-def KAA(intro,**data):
-    print("\n",intro)
-    print(data)
-KAA("Keyworded Arbitrary Arguments stores as a dictionary",name="Sarim",roll_no=812,marks=872,percentage=93.47)
+# arbitrary positional argument
+def area_of_circle(*marks):
+    total=sum(marks)
+    print("\n",marks,"\n","Total_Marks= ",total)
+area_of_circle(23,55,76,87,90)
+'''
+Prefixed with a single asterisk(*), this collects any number
+of extra positional arguments into a sinngle tuple.
+'''
+
+# arbitrary keyword argument
+def info(**data):
+    print("\n",data)
+info(roll_no=812,marks=872,percentage=93.47)
+'''
+Prefixed with a double asterisk(**), this collects any number
+of extra keyword arguments into a standard dictionary
+'''
+
+# positional+arbitray keyword arguments
+def info(name,**data):
+    print("\n",name," = ",data)
+info("Sarim",roll_no=812,marks=872,percentage=93.47)
+
+# positional+arbitray posional arguments
+def info(box_num,*fruit):
+    print("\n",box_num," = ",fruit)
+info(107,"Apple","Mango","Dates","Orange")
