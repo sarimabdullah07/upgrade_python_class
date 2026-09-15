733e7d4aa65c5454446f2ac5a1c62934a50ff872
sarimabdullah07 <sarimabdullah242@gmail.com>
2026-09-15 17:31:08 +0530

    fibonacci series

---
+++ recursion_function.py
@@ -0,0 +1,12 @@
+#recursion function fibonacci series
+def fibonacci(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        f=fibonacci(n-1)+fibonacci(n-2)
+    return f
+l=int(input("Enter number of terms for fibonacci series: "))
+for i in range(l):
+    print(fibonacci(i),end=" ")
